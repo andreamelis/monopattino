829b6c75c1295d802d3caa8ca2e8d9a44dc09af1
Andrea Melis <61542902+andreamelis@users.noreply.github.com>
2020-06-07 00:42:17 +0200

    prova

--- php/login.php
+++ php/login.php
@@ -17,9 +17,10 @@ if ($conn->connect_error) {
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
@@ -31,7 +32,7 @@ if ($conn->connect_error) {
 
 <body>
 
-<div class="jumbotron text-center text-white bg-primary titolo">
+  <div class="jumbotron text-center text-white bg-primary titolo">
     <h1>Monopattiniamo.it</h1>
     <h5>Il sito dove troverai i migliori monopattini ai prezzi più bassi.</h5>
     <nav class="navbar navbar-expand-sm bg-success navbar-secondary justify-content-center">
